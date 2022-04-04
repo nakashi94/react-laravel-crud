6f3430d19a3abb67d49ca94f2517da9c030286ff
nakashi94 <n0904yBUNJOU@gmail.com>
2022-04-05 00:15:03 +0900

    create dumy taskdata with seeder

---
+++ api/database/seeders/TaskSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class TaskSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table("tasks")->insert([
+            "content" => Str::random(10),
+        ]);
+    }
+}
