b5009ee59e7a62352fa6f752a47b744ab5da9511
nakashi94 <n0904yBUNJOU@gmail.com>
2022-04-05 00:41:24 +0900

    get taskdata from db

--- front/src/components/pages/Tasks.tsx
+++ front/src/components/pages/Tasks.tsx
@@ -1,10 +1,19 @@
+import axios from "axios";
 import { memo, VFC } from "react";
 
 export const Tasks: VFC = memo(() => {
-    
+    const api = axios.create({
+        baseURL: "http://localhost:8000",
+    });
+    const getTasks = () => {
+        api.get("/api/tasks")
+            .then(res => console.log(res.data))
+            .catch(err => console.log(err))
+    };
     return (
         <>
             <p>Tasks</p>
+            <button onClick={getTasks} >getTasks</button>
         </>
     )
 })
