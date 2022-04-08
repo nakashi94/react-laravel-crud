f6721d0ba76b1a4d38a7ed33d8d75dc2f855d201
nakashi94 <n0904yBUNJOU@gmail.com>
2022-04-08 20:49:20 +0900

    initialize front directory

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,15 +1,6 @@
-import { BrowserRouter } from "react-router-dom"
-import { Router } from "./router/Router";
-import { ChakraProvider } from "@chakra-ui/react";
-import { theme } from "./theme/theme";
-
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-    </ChakraProvider>
+    <>App</>
   );
 }
 
--- front/src/types/task.ts
+++
@@ -1,6 +0,0 @@
-export type Task = {
-    id: number;
-    content: string;
-    created_at: string; 
-    updated_at: string;
-}
